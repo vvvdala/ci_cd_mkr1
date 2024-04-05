9bc4f61a3db1600a01b8c7257e9c6e7f51b7d393
Veronka <veronkievipad@gmail.com>
2024-04-05 12:33:05 +0300

    ended task

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+def filter_text(input_path: str, output_path: str, keyword: str):
+    """
 
+        Parameters:
+        input_path (str): The path to the input text file to be processed.
+        output_path (str): The path to the file where the filtered text will be saved.
+        keyword (str): The keyword for filtering.
 
-def filter_text(input_path, output_path, keyword):
-    with open(input_path, 'r') as input_file, open(output_path, 'w') as output_file:
+        Returns:
+        None
+        """
+    with open(input_path, 'r', encoding='utf-8') as input_file, open(output_path, 'w', encoding='utf-8') as output_file:
         for line in input_file:
             if keyword in line:
                 output_file.write(line)
 
+input_path = 'my_text_file.txt'
+output_path = 'filtered.txt'
+
+keyword = 'перевірка'
+
+filter_text(input_path, output_path, keyword)
+
+print("Результат збережено у файлі 'filtered.txt'.")
 
 
 
