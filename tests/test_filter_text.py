08ac586cb1f838d16175ec180d8ba9e1dc33fb42
Veronka <veronkievipad@gmail.com>
2024-04-05 12:43:23 +0300

    one test using fixture realized

--- tests/test_filter_text.py
+++ tests/test_filter_text.py
@@ -17,7 +17,21 @@ def temp_file(tmp_path):
 
     return file_path
 
-def test_filter_text_fix():
+def test_filter_text_fix(temp_file):
+    keyword = 'перевірка'
+    output_path = temp_file.parent/'filtered.txt'
+
+    filter_text(temp_file, output_path, keyword)
+
+    with open(output_path, 'r', encoding='utf-8') as filtered_file:
+        filtered_content = filtered_file.read()
+
+    assert keyword in filtered_content
+
+    os.remove(output_path)
+
+
+
 
 
 
